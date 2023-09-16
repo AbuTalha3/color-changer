63eacb4957216ad97b3cf584e0883a22e6bf642e
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-09-16 12:57:39 +0500

    Color changing page

---
+++ index.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+btn.addEventListener("click", () => {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+});
